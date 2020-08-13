180b94993851c77c8248fae6d629566752d4eee1
MostafaElebzary <mostafaelebzary@gmail.com>
2020-08-14 00:51:43 +0200

    bezra

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         date_default_timezone_set('Africa/Cairo');
-//        Paginator::useTailwind();
+        Paginator::viewFactory();
 
     }
 }
